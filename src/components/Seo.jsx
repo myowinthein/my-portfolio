c1da9453442a96b84497a66975f0253eb8f0712a
Myo Win Thein (Martin) <mwt.creative@gmail.com>
2026-07-10 01:39:46 +0700

    refactor(quality): remove heroContent indirection, fix Twitter meta tags, remove IE meta, drop outer fragment

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -11,40 +11,35 @@ const personSchema = JSON.stringify({
 });
 
 const SEO = ({ pageTitle }) => (
-  <>
-    <Head>
-      <title>
-        {pageTitle ? `${pageTitle} | ${position}` : metaTitle}
-      </title>
+  <Head>
+    <title>{pageTitle ? `${pageTitle} | ${position}` : metaTitle}</title>
 
-      {/* Primary Meta Tags */}
-      <meta name="description" content={metaDescription} />
-      <meta name="robots" content="index, follow" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <link rel="canonical" href={siteURL} />
+    {/* Primary Meta Tags */}
+    <meta name="description" content={metaDescription} />
+    <meta name="robots" content="index, follow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="canonical" href={siteURL} />
 
-      {/* Open Graph / Facebook */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={siteURL} />
-      <meta property="og:title" content={metaTitle} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:image" content={metaImage} />
+    {/* Open Graph / Facebook */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={siteURL} />
+    <meta property="og:title" content={metaTitle} />
+    <meta property="og:description" content={metaDescription} />
+    <meta property="og:image" content={metaImage} />
 
-      {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content={siteURL} />
-      <meta property="twitter:title" content={metaTitle} />
-      <meta property="twitter:description" content={metaDescription} />
-      <meta property="twitter:image" content={metaImage} />
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    {/* Twitter */}
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:url" content={siteURL} />
+    <meta name="twitter:title" content={metaTitle} />
+    <meta name="twitter:description" content={metaDescription} />
+    <meta name="twitter:image" content={metaImage} />
 
-      {/* Structured Data */}
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: personSchema }} />
+    {/* Structured Data */}
+    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: personSchema }} />
 
-      {/* Favicon */}
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-    </Head>
-  </>
+    {/* Favicon */}
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  </Head>
 );
 
 export default SEO;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,44 +4,37 @@ import heroImage from "../../../public/assets/img/hero/dark.jpg";
 import heroImgMobile from "../../../public/assets/img/hero/img-mobile.jpeg";
 import Image from "next/image";
 
-const heroContent = {
-  heroImage: heroImage,
-  heroMobileImage: heroImgMobile,
-  heroTitleName: `${firstName} ${lastName}`,
-  heroDesignation: position,
-  heroDescriptions: summary,
-  heroRoleTags: roleTags,
-  heroBtn: "View Resume",
-};
+const fullName = `${firstName} ${lastName}`;
 
 const Hero = () => {
   return (
     <div className="row home-details-container align-items-center">
       <div
         className="col-lg-4 bg position-fixed d-none d-lg-block"
-        style={{ backgroundImage: `url(${heroContent.heroImage.src})` }}
+        style={{ backgroundImage: `url(${heroImage.src})` }}
       ></div>
       <div className="col-12 col-lg-8 offset-lg-4 home-details  text-center text-lg-start">
         <div>
           <Image
-            src={heroContent.heroMobileImage}
+            src={heroImgMobile}
             className="img-fluid main-img-mobile d-sm-block d-lg-none"
-            alt={heroContent.heroTitleName}
+            alt={fullName}
+            priority
           />
           <h1 className="poppins-font">
-            <span className="d-block text-uppercase">{heroContent.heroTitleName}</span>
-            <span>{heroContent.heroDesignation}</span>
+            <span className="d-block text-uppercase">{fullName}</span>
+            <span>{position}</span>
           </h1>
           <div className="hero-credentials open-sans-font">
-            {heroContent.heroRoleTags.map((tag) => (
-              <span className="credential-item" key={tag}>{tag}</span>
+            {roleTags.map((tag, i) => (
+              <span className="credential-item" key={i}>{tag}</span>
             ))}
           </div>
-          {heroContent.heroDescriptions.map((description) => (
-            <p className="open-sans-font" key={description}>{description}</p>
+          {summary.map((description, i) => (
+            <p className="open-sans-font" key={i}>{description}</p>
           ))}
           <a className="button" href={resumeURL} target="_blank" rel="noreferrer">
-            <span className="button-text">{heroContent.heroBtn}</span>
+            <span className="button-text">View Resume</span>
             <span className="button-icon fa fa-download"></span>
           </a>
         </div>
